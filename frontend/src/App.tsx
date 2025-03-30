59b540202eff433555fdf32896e21841b21f183e
Kacper WW <kacper1c26@gmail.com>
2025-03-30 17:10:00 +0200

    feat: Add Routes and more tailwind things

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DefaultPage from "./pages/DefaultPage";
+import Login from "./pages/Login";
+import MainPage from "./pages/MainPage";
+import PrivateRoute from "./routes/PrivateRoute";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<DefaultPage />} />
+          <Route path="/login" element={<Login />} />
+          {/*<Route path="/product/:id" element={<Register />} />*/}
+          <Route element={<PrivateRoute />}>
+          <Route path="/main" element={<MainPage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
