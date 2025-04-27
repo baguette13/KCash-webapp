1e616a1c384bc25eeb20c573023db3e4c9f220e7
Kacper WW <kacper1c26@gmail.com>
2025-04-27 16:42:51 +0200

    fix: fixed endpoints and views after refactoring

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,14 +3,20 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 from api import views
+from api.controllers import order_controller, product_controller, user_controller
 
 
 urlpatterns = [
-    path('products/', views.product_list, name='product_list'),
-    path('orders/', views.order_list, name='order_list'),
+    #path('products/', views.product_list, name='product_list'),
+    #path('orders/', views.order_list, name='order_list'),
     path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('profile/details/', views.profile_details, name='profile_details'),
-    path('profile/update/', views.update_profile, name='update_profile'),
-    path('orders/history/', views.get_user_orders, name='order-history'),
+    #path('profile/details/', views.profile_details, name='profile_details'),
+    #path('profile/update/', views.update_profile, name='update_profile'),
+    #path('orders/history/', views.get_user_orders, name='order-history'),
+    path('orders/history/', order_controller.get_user_orders, name='order-history'),
+    path('products/', product_controller.product_list, name='product_list'),
+    path('orders/', order_controller.order_list, name='order_list'),
+    path('profile/details/', user_controller.profile_details, name='profile_details'),
+    path('profile/update/', user_controller.update_profile, name='update_profile'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,69 +1,79 @@
-from rest_framework import viewsets, permissions
-from rest_framework.response import Response
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .models import Product, Order, ClientUser
-from .serializers import ProductSerializer, OrderSerializer, UserSerializer
-from django.contrib.auth import get_user_model
-from django.http import JsonResponse
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+# from rest_framework import viewsets, permissions
+# from django.core.validators import validate_email
+# from django.core.exceptions import ValidationError
+# from rest_framework.response import Response
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+# from .models import Product, Order, ClientUser
+# from .serializers import ProductSerializer, OrderSerializer, UserSerializer
+# from django.contrib.auth import get_user_model
+# from django.http import JsonResponse
+# from rest_framework import status
+# from rest_framework.decorators import api_view, permission_classes
+# from rest_framework.permissions import IsAuthenticated
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['GET', 'POST'])
+# def product_list(request):
+#     if request.method == 'GET':
+#         products = Product.objects.all()
+#         serializer = ProductSerializer(products, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = ProductSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticated])
-def order_list(request):
-    if request.method == 'GET':
-        orders = Order.objects.filter(user=request.user).order_by('-created_at')
-        serializer = OrderSerializer(orders, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = OrderSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)  # Przypisz zamówienie do aktualnie zalogowanego użytkownika
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['GET', 'POST'])
+# @permission_classes([IsAuthenticated])
+# def order_list(request):
+#     if request.method == 'GET':
+#         orders = Order.objects.filter(user=request.user).order_by('-created_at')
+#         serializer = OrderSerializer(orders, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST': 
+#         serializer = OrderSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save(user=request.user)  # Przypisz zamówienie do aktualnie zalogowanego użytkownika
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def profile_details(request):
-    user = request.user  # Zaciąganie zalogowanego użytkownika
-    serializer = UserSerializer(user)
-    return Response(serializer.data)
+# @api_view(["GET"])
+# @permission_classes([IsAuthenticated])
+# def profile_details(request):
+#     user = request.user  # Zaciąganie zalogowanego użytkownika
+#     serializer = UserSerializer(user)
+#     return Response(serializer.data)
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def update_profile(request):
-    user = request.user  # Zaciąganie zalogowanego użytkownika
+# @api_view(['POST'])
+# @permission_classes([IsAuthenticated])
+# def update_profile(request):
+#     user = request.user  # Zaciąganie zalogowanego użytkownika
 
-    # Odczyt danych z żądania
-    data = request.data
-    user.first_name = data.get('first_name', user.first_name)
-    user.last_name = data.get('last_name', user.last_name)
-    user.username = data.get('username', user.username)
-    user.email = data.get('email', user.email)
+#     # Odczyt danych z żądania
+#     data = request.data
 
-    # Zapisanie zaktualizowanych danych
-    user.save()
+#     # Walidacja emaila
+#     email = data.get('email', user.email)
+#     try:
+#         validate_email(email)
+#     except ValidationError:
+#         return JsonResponse({'error': 'Invalid email address'}, status=400)
 
-    return JsonResponse({'message': 'Profile updated successfully'})
+#     user.first_name = data.get('first_name', user.first_name)
+#     user.last_name = data.get('last_name', user.last_name)
+#     user.username = data.get('username', user.username)
+#     user.email = data.get('email', user.email)
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def get_user_orders(request):
-    user=request.user
-    user_orders = Order.objects.filter(user=user).order_by('-created_at')
-    serializer = OrderSerializer(user_orders, many=True)
-    return Response(serializer.data)
+#     # Zapisanie zaktualizowanych danych
+#     user.save()
+
+#     return JsonResponse({'message': 'Profile updated successfully'})
+
+# @api_view(["GET"])
+# @permission_classes([IsAuthenticated])
+# def get_user_orders(request):
+#     user=request.user
+#     user_orders = Order.objects.filter(user=user).order_by('-created_at')
+#     serializer = OrderSerializer(user_orders, many=True)
+#     return Response(serializer.data)
