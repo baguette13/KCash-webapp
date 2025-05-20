b97a85775bd298aa8f824f7b4d88d3c760035df3
Kacper WW <kacper1c26@gmail.com>
2025-05-20 15:10:24 +0200

    fix: handling orders in order_repository

--- backend/api/repositories/order_repository.py
+++ backend/api/repositories/order_repository.py
@@ -3,7 +3,14 @@ from api.models import Order, OrderItem, Product
 class OrderRepository:
     @staticmethod
     def get_orders_by_user(user):
-        return Order.objects.filter(user=user).order_by('-created_at')
+        return Order.objects.filter(user=user).prefetch_related('orderitem_set__product').order_by('-created_at')
+
+    @staticmethod
+    def get_order_with_products(order_id):
+        """
+        Pobierz zamówienie wraz z powiązanymi produktami
+        """
+        return Order.objects.prefetch_related('orderitem_set__product').get(id=order_id)
 
     @staticmethod
     def create_order(user, validated_data):
@@ -14,7 +21,12 @@ class OrderRepository:
         
         # Tworzenie pozycji zamówienia w modelu pośrednim OrderItem
         for product_data in products_data:
-            product = Product.objects.get(id=product_data['product']['id'])
-            OrderItem.objects.create(order=order, product=product, quantity=product_data['quantity'])
+            product_id = product_data.get('product', {}).get('id')
+            quantity = product_data.get('quantity', 1)
+            try:
+                product = Product.objects.get(id=product_id)
+                OrderItem.objects.create(order=order, product=product, quantity=quantity)
+            except Product.DoesNotExist:
+                print(f"Product with id {product_id} does not exist")
         
         return order
