4a6807fe4efa0c83f88bd755a799c66987f9dc3f
Kacper WW <kacper1c26@gmail.com>
2025-06-02 23:42:19 +0200

    feat: integration of session monitoring system with main app component

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,10 +1,37 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import FormWrapper from "../wrappers/FormWrapper";
 import LoginForm from "../components/authorization/main-components/LoginForm";
+import SessionExpiredNotification from "../components/notification/SessionExpiredNotification";
+import SessionExpiredDialog from "../components/notification/SessionExpiredDialog";
 
 const Login: FC = () => {
+  const [showSessionExpired, setShowSessionExpired] = useState(false);
+  const [showSessionExpiredDialog, setShowSessionExpiredDialog] = useState(false);
+  
+  useEffect(() => {
+    const sessionExpired = sessionStorage.getItem('sessionExpired');
+    if (sessionExpired === 'true') {
+      const sessionExpiredSource = sessionStorage.getItem('sessionExpiredSource');
+      
+      if (sessionExpiredSource === 'dialog') {
+        setShowSessionExpiredDialog(true);
+      } else {
+        setShowSessionExpired(true);
+      }
+      
+      sessionStorage.removeItem('sessionExpired');
+      sessionStorage.removeItem('sessionExpiredSource');
+    }
+  }, []);
+
   return (
     <FormWrapper>
+      {showSessionExpired && (
+        <SessionExpiredNotification onClose={() => setShowSessionExpired(false)} />
+      )}
+      {showSessionExpiredDialog && (
+        <SessionExpiredDialog onClose={() => setShowSessionExpiredDialog(false)} />
+      )}
       <LoginForm/>
     </FormWrapper>
   );
